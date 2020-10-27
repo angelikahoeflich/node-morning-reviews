63e20b29eb74d4427f43b02f8ff02c562a86a62c
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-27 09:00:49 -0700

    accessed database!

--- server/index.js
+++ server/index.js
@@ -10,6 +10,37 @@ app.get('api/users', (req,res) => {
   res.status(200).send(users)
 })
 
+app.get('/api/users:id', (req, res) => { const {id} = req.params 
+    if(!id)
+      return res.status(404).send('unable to find resource')
+
+      const foundUser = users.find(user => user.id === +id)
+
+      if(!foundUser){
+        return res.status(500).send('unable to find user')
+      }
+      res.status(200).send(foundUser)
+})
+
+//query
+//axios.post('api/users?name=angelika')
+app.post('/api/users', (req, res) => {
+  const {name} = req.query;
+})
+
+//axios.post('/api/users/cole')
+//params
+app.post('/api/users', (req, res) => {
+  const {name} = req.params;
+})
+
+//axios.post('api/users', {name:cole})
+//body
+app.post('/api/users', (req, res) => {
+  const {name} = req.body
+})
+
+
 app.listen(port, () => {
   console.log(`server running on ${port}`)
 });
